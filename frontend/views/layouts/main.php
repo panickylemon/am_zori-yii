35dca42b244a596ac2b04a50286ca522ddd7dc74
panickylemon <alenka__1992@mail.ru>
2016-03-07 20:45:30 +0300

    4

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -28,7 +28,7 @@ AppAsset::register($this);
 
 <div class="header">
 	<div class="header_layout">
-		<div><a href="<?= Url::toRoute(['page/index']) ?>"><img src="../pictures/logo.png" alt="логотип
+		<div><a href="<?= Url::toRoute(['page/index']) ?>"><img src="/frontend/web/pictures/logo.png" alt="логотип
         Амурские зори" class="logo"></a></div>
 
 		<div class="menu">
@@ -49,7 +49,7 @@ AppAsset::register($this);
 
 
 			<div class="menu_burger">
-				<a href="#" onclick='$(".menu_hidden").slideToggle("slow");'><img src="/frontend/web/pictures/menu55.png"
+				<a href="#" onclick='$(".menu_hidden").slideToggle("slow");'><img src="../pictures/menu55.png"
 				                                                                  alt="меню" class="burger"></a>
 			</div>
 		</div>
