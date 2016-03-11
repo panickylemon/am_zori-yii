8b94a870d1f04e4ced8e16d8cfefec101af406a8
panickylemon <alenka__1992@mail.ru>
2016-03-11 16:45:55 +0300

    6

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -50,9 +50,9 @@ SliderAsset::register($this); ?>
 </script>
 
 
-<div class="bxslider">
-    <ul class="bxslider_main">
-        <li><img class="item" src="../pictures/image1.png" alt="земляничные поляны">
+<ul class="bxslider">
+    <div class="bxslider_main">
+        <li><img class="item" src="/frontend/web/pictures/image1.png">
 
             <div class="container_text_slider">
                 <h1>Земляничные поляны</h1>
@@ -62,7 +62,7 @@ SliderAsset::register($this); ?>
                     оптимальный вариант приобретения жилья по экономичной цене</h2>
             </div>
         </li>
-        <li><img class="item" src="../pictures/image2.png" alt="pощинская слобода">
+        <li><img class="item" src="/frontend/web/pictures/image2.png">
 
             <div class="container_text_slider">
                 <h1>Pощинская слобода</h1>
@@ -72,7 +72,7 @@ SliderAsset::register($this); ?>
                     городом</h2>
             </div>
         </li>
-        <li><img class="item" src="../pictures/image3.png" alt="медовое">
+        <li><img class="item" src="/frontend/web/pictures/image3.png">
 
             <div class="container_text_slider">
                 <h1>Медовое</h1>
@@ -82,48 +82,42 @@ SliderAsset::register($this); ?>
                     Орельского озера и песчаного карьера — прекрасного места для летнего купания</h2>
             </div>
         </li>
-    </ul>
-</div>
+    </div>
+</ul>
 
 
 <div class="content">
     <div class="content_layout">
-        <div class="icon_mainpage">
-            <a href="<?= Url::toRoute(['page/village']) ?>">
-                <div class="box_icon_picture"><img class="pictire_icon" src="../pictures/icon_localization.png"
-                                                   alt="выбрать поселок"></div>
+        <div class="icon_mainpage"><a href="<?= Url::toRoute(['page/village']) ?>">
+                <div class="box_icon_picture"><img class="pictire_icon" src="/frontend/web/pictures/icon_localization.png"></div>
                 <div class="box_text_icon">
                     <div class="text_icon_title">Выбрать посёлок</div>
                     <div class="subtext_icon">Коттеджные посёлки от эконом-класса до премиум-класса</div>
                 </div>
-            </a>
         </div>
-
+        </a>
 
         <div class="icon_mainpage">
             <a href="<?= Url::toRoute(['selection/selection']) ?>">
-                <div class="box_icon_picture"><img class="pictire_icon" src="../pictures/icon_home.png"
-                                                   alt="подобрать участок"></div>
+                <div class="box_icon_picture"><img class="pictire_icon" src="/frontend/web/pictures/icon_home.png"></div>
                 <div class="box_text_icon">
                     <div class="text_icon_title">Подобрать участок</div>
                     <div class="subtext_icon">Приобретите загородный участок и избавьтесь от городской суеты, шума и
                         пыли
                     </div>
                 </div>
-            </a>
         </div>
-
+        </a>
 
         <div class="icon_mainpage">
             <a href="<?= Url::toRoute(['page/how_buy']) ?>">
-                <div class="box_icon_picture"><img class="pictire_icon" src="../pictures/icon_wallet.png"
-                                                   alt="как купить"></div>
+                <div class="box_icon_picture"><img class="pictire_icon" src="/frontend/web/pictures/icon_wallet.png"></div>
                 <div class="box_text_icon">
                     <div class="text_icon_title">Как купить</div>
                     <div class="subtext_icon">Множество вариантов приобретения участка, а также скидки и акции</div>
                 </div>
-            </a>
         </div>
+        </a>
     </div>
 
 </div>
@@ -145,8 +139,8 @@ SliderAsset::register($this); ?>
             требовательному
             Клиенту комплексный подход в области строительства и всех операций с недвижимостью.
         </div>
-        <p class="five_reasons">5 причин купить недвижимость у нас:</p>
-        <ul>
+        <ul>5 причин купить недвижимость у нас:
+
             <li>Лучшие цены</li>
             <li>Хорошая транспортная доступность</li>
             <li>Развитая инфраструктура</li>
@@ -186,31 +180,31 @@ SliderAsset::register($this); ?>
         <div class="bxslider bxslider2">
             <div class="slider_right">
 
-                <div><a href="#"><img class="item" src="../pictures/sale1.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale1.png" alt="галерея"></a>
                 </div>
 
-                <div><a href="#"><img class="item" src="../pictures/sale2.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale2.png"></a>
                 </div>
 
-                <div><a href="#"><img class="item" src="../pictures/sale3.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale3.png"></a>
                 </div>
 
-                <div><a href="#"><img class="item" src="../pictures/sale4.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale4.png"></a>
                 </div>
 
-                <div><a href="#"><img class="item" src="../pictures/sale5.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale5.png"></a>
                 </div>
 
-                <div><a href="#"><img class="item" src="../pictures/sale6.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale6.png"></a>
                 </div>
 
-                <div><a href="#"><img class="item" src="../pictures/sale7.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale7.png"></a>
                 </div>
 
-                <div><a href="#"><img class="item" src="../pictures/sale8.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale8.png"></a>
                 </div>
 
-                <div><a href="#"><img class="item" src="../pictures/sale9.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale9.png"></a>
                 </div>
             </div>
         </div>
@@ -221,31 +215,31 @@ SliderAsset::register($this); ?>
         <div class="bxslider bxslider2">
             <div class="slider_bottom">
 
-                <div><a href="#"><img class="item" src="../pictures/sale1.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale1.png"></a>
                 </div>
 
-                <div><a href="#"><img class="item" src="../pictures/sale2.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale2.png"></a>
                 </div>
 
-                <div><a href="#"><img class="item" src="../pictures/sale3.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale3.png"></a>
                 </div>
 
-                <div><a href="#"><img class="item" src="../pictures/sale4.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale4.png"></a>
                 </div>
 
-                <div><a href="#"><img class="item" src="../pictures/sale5.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale5.png"></a>
                 </div>
 
-                <div><a href="#"><img class="item" src="../pictures/sale6.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale6.png"></a>
                 </div>
 
-                <div><a href="#"><img class="item" src="../pictures/sale7.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale7.png"></a>
                 </div>
 
-                <div><a href="#"><img class="item" src="../pictures/sale8.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale8.png"></a>
                 </div>
 
-                <div><a href="#"><img class="item" src="../pictures/sale9.png" alt="галерея"></a>
+                <div><a href="#"><img class="item" src="/frontend/web/pictures/sale9.png"></a>
                 </div>
             </div>
             <div class="link_to_galery"><a href="#"> Перейти в галерею >></a></div>
